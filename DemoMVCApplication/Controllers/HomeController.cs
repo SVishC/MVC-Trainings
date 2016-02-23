32188b7ca2394360f8a6373a68b4b6155c7e4230
SVishC <chaitanyamvishnu@gmail.com>
2016-02-23 09:04:39 +0530

    Demo MVC Application for ViewBag and Viewdata commited

---
+++ DemoMVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVCApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            /*
+#region ViewBag_Countries
+            ViewBag.Countries = new List<string>() { 
+            "India","Pakistan","Srilanka","Bangladesh"
+            };
+#endregion 
+             */
+            #region viewData_Countries
+            ViewData["Countries"] = new List<string>() { 
+            "India","Pakistan","Srilanka","Bangladesh"
+            };
+            #endregion
+            return View();
+        }
+
+    }
+}
