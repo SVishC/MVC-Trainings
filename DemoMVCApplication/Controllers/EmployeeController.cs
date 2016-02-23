d90d70312a2b4d312fb934368b4e98e3988bcf80
SVishC <chaitanyamvishnu@gmail.com>
2016-02-23 14:28:55 +0530

    Employee Model and Employee Controller and Details View added

---
+++ DemoMVCApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoMVCApplication.Models;
+
+namespace DemoMVCApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult Details()
+        {
+            Employee employee = new Employee() { EmployeeId = 1, Name = "Sebastien", Gender = "Male", City = "Newyork" };
+     
+            return View(employee);
+        }
+
+    }
+}
